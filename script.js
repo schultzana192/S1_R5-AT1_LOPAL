37515eeb0c3dad536846b8957f68020cdb662c7b
Ana Clara <schultzana192@gmail.com>
2026-05-06 09:49:39 -0300

    realizado a ativiade de tabuada

--- script.js
+++ script.js
@@ -1,7 +1,12 @@
-let soma = 0; 
-// variavel para identificar a soma
+let numero = Number(prompt("insira um número"));
 
-for (let i = 1; i <= 10; i++) {
-    soma += i ;
+if (isNaN(numero)) {
+    alert("insira um número verdadeiro");
+} else {
+   let mensaGem = `tabuada do ${numero}`;
+
+   for (let i = 1; i<= 10; i= i+1) {
+    mensaGem += `${numero} x ${i} = ${numero * i} / `;
+ }
+ alert(mensaGem);
 }
-alert (soma)
