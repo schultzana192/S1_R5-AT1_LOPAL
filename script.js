8e23633c2ad83b7b162ba245b0707d14d0a6ebf7
Ana Clara <schultzana192@gmail.com>
2026-05-06 10:44:19 -0300

    realizado o exercicio 5

--- script.js
+++ script.js
@@ -1,18 +1,20 @@
-let escolha
+let soma = 0;
+let quantidade = 0;
+let entrada;
 
 do {
-    escolha = Number(prompt("Escolha uma opção: 1 - insira o produto \ 2 - lista de produtos \ 3 - Sair"));
-    switch (escolha) {
-        case 1:
-            // o menu continua aparecendo
-            break;
-        case 2:
-            // o menu continua aparecendo
-            break;
-        case 3:
-            alert("Sistema encerrado");
-            break;
-        default:
-            alert("Opção inválida");
-    }
-} while (escolha !== 3); 
+     entrada =  Number(prompt('Digite um número'));
+   
+    if(!isNaN (entrada) && entrada !== 0) {
+        soma += entrada;
+        quantidade += 1;
+ 
+        }
+    } while (entrada !== 0);
+
+    let media = soma / quantidade;
+    
+    alert (`quantidade de números digitados: ${quantidade};Soma dos números: ${soma};Média dos números: ${media}`);
+
+
+
