b98d383b04c0efd77e108ffd39f697dac5ce0b6e
Ana Clara <schultzana192@gmail.com>
2026-05-06 10:09:40 -0300

    realizado o exercicio de compra

--- script.js
+++ script.js
@@ -1,12 +1,18 @@
-let numero = Number(prompt("insira um número"));
+let escolha
 
-if (isNaN(numero)) {
-    alert("insira um número verdadeiro");
-} else {
-   let mensaGem = `tabuada do ${numero}`;
-
-   for (let i = 1; i<= 10; i= i+1) {
-    mensaGem += `${numero} x ${i} = ${numero * i} / `;
- }
- alert(mensaGem);
-}
+do {
+    escolha = Number(prompt("Escolha uma opção: 1 - insira o produto \ 2 - lista de produtos \ 3 - Sair"));
+    switch (escolha) {
+        case 1:
+            // o menu continua aparecendo
+            break;
+        case 2:
+            // o menu continua aparecendo
+            break;
+        case 3:
+            alert("Sistema encerrado");
+            break;
+        default:
+            alert("Opção inválida");
+    }
+} while (escolha !== 3); 
